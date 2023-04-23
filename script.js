a5b361b2753ae072e96cba486152cdde3a823b94
bhargavsai15 <36848893+bhargavsai15@users.noreply.github.com>
2023-04-23 11:28:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const browser = document.getElementById('browser');
+const version = document.getElementById('version');
+window.addEventListener('load', () => {
+  browser.innerText = navigator.appName;
+  version.innerText = navigator.appVersion;
+})
